413d86fc665cf1c1b01ee02531d58629b61a0ad9
Pavel B <pavel.b@smartum.pro>
2018-09-09 13:01:43 +0300

    login

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,120 +1,47 @@
 import * as React from 'react';
-import Menu from './Menu';
+import HDWalletProvider from 'truffle-hdwallet-provider';
+import Web3 from 'web3';
 import { ViewsEnum } from '../views.enum';
-import { Oracles } from './Oracles';
 import './app.css';
-import { CurveInit } from './CurveInit';
-import { GetEndpoint } from './GetEndpoint';
-import { Bondage } from './Bondage';
-import { getZap, getEth } from '../subscriber';
-import { ZapProvider } from '@zapjs/provider/lib/src';
-import { Unbondage } from './Unbondage';
-import { Query } from './Query';
-import { CreateProvider } from './CreateProvider';
+import { Admin } from './Admin';
+import { providerUrl } from '../config';
+import { loadAccount, loadProvider } from '../utils';
 
-interface AppProps {
-  defaultProvider: ZapProvider;
-  address: string;
-  web3: any;
-  defaultView: ViewsEnum;
-}
-
-interface AppState {
-  view: ViewsEnum;
-  provider: ZapProvider;
-}
-
-export class App extends React.Component<AppProps, AppState> {
-  constructor(props) {
-    super(props);
-    this.state = { view: props.defaultView, provider: this.props.defaultProvider};
-    this.handleMenuChange = this.handleMenuChange.bind(this);
-  }
-  handleMenuChange(view) {
-    this.setState({view});
+export class App extends React.Component {
+  state = {web3: null, address: null, provider: null, loading: false, error: null};
+  async handleLogin(e) {
+    e.preventDefault();
+    this.setState({error: null, loading: true, provider: null, address: null});
+    try {
+      const web3: any = new Web3(new HDWalletProvider(e.target.mnemonic.value, providerUrl));
+      const address = await loadAccount(web3);
+      const provider = await loadProvider(web3, address);
+      await provider.getTitle();
+      this.setState({web3, address, provider, loading: false});
+    } catch (e) {
+      this.setState({loading: false, error: e.message});
+    }
   }
   render() {
-    const { address, web3 } = this.props;
-    const { view, provider } = this.state;
+    const { address, web3, provider, loading, error } = this.state;
     return (
-      <React.Fragment>
-        <header>
-          <Address address={address}></Address>
-          <Provider providerTitle={provider.title}></Provider>
-        </header>
-        <main>
-          <aside>
-            <Menu view={view} providerTitle={provider.title} onMenuChange={this.handleMenuChange}></Menu>
-          </aside>
-          <MainSection address={address} web3={web3} view={view}></MainSection>
-        </main>
-      </React.Fragment>
+      provider && !loading && !error
+        ? <Admin
+            web3={web3}
+            address={address}
+            defaultProvider={provider}
+            defaultView={ViewsEnum.MAIN}
+            />
+        : <form className={loading ? 'disabled' : undefined} onSubmit={e => {this.handleLogin(e)}}>
+            {!!error && <div className="message message-error">{error}</div>}
+            <div className="form-group">
+              <label htmlFor="mnemonic">Mnemonic</label>
+              <input type="text" name="mnemonic" id="mnemonic"/>
+            </div>
+            <div className="form-group">
+              <button type="submit">Login</button>
+            </div>
+          </form>
     );
   }
 };
-
-class MainSection extends React.PureComponent<{address: string; view: ViewsEnum; web3: any}> {
-  render() {
-    const {address, view, web3} = this.props;
-    let main = null;
-    let className = view ? view.toLowerCase() : undefined;
-    switch (view) {
-      case ViewsEnum.MAIN:
-        main = <Info address={address} web3={web3} ></Info>;
-        break;
-      case ViewsEnum.ORACLES:
-        main = <Oracles web3={web3}></Oracles>;
-        break;
-      case ViewsEnum.INIT_CURVE:
-        main = <CurveInit web3={web3} address={address}></CurveInit>;
-        break;
-      case ViewsEnum.GET_ENDPOINT:
-        main = <GetEndpoint web3={web3} address={address}></GetEndpoint>;
-        break;
-      case ViewsEnum.BONDAGE:
-        main = <Bondage web3={web3} address={address}></Bondage>;
-        break;
-      case ViewsEnum.UNBONDAGE:
-        main = <Unbondage web3={web3} address={address}></Unbondage>;
-        break;
-      case ViewsEnum.QUERY:
-        main = <Query web3={web3} address={address}></Query>;
-        break;
-      case ViewsEnum.CREATE_PROVIDER:
-        main = <CreateProvider web3={web3} address={address}></CreateProvider>;
-        break;
-    }
-    return <section className={className}>{main}</section>;
-  }
-}
-
-const Address = ({address}) => (
-  <div className="address">Address: {address}</div>
-);
-
-class Info extends React.PureComponent<{address: string, web3: any}> {
-  state = {eth: '', zap: ''};
-  componentDidMount() {
-    const {web3, address} = this.props;
-    getZap(web3, address).then(zap => { this.setState({zap}); })
-    getEth(web3, address).then(eth => { this.setState({eth}); })
-  }
-  render() {
-    const {eth, zap} = this.state;
-    return (
-      <React.Fragment>
-        <div>Address: {this.props.address}</div>
-        <div>ETH Balance: {eth} wei</div>
-        <div>ZAP Balance: {zap} wei ZAP</div>
-      </React.Fragment>
-    )
-  }
-}
-
-const Provider = ({providerTitle}) => (
-  <div>
-    {providerTitle ? 'Found provider: ' + providerTitle : 'This account is currently not setup as a provider'}
-  </div>
-);
-
-export default App;
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,8 @@
-import HDWalletProvider from 'truffle-hdwallet-provider';
-import Web3 from 'web3';
-import { loadAccount, loadProvider } from './utils';
-import { providerUrl } from './config';
+import { createElement } from 'react';
 import { render } from 'react-dom';
-import App from './app/App';
-import * as React from 'react';
-import { ViewsEnum } from './views.enum';
+import { App } from './app/App';
 
-const mnemonic = 'silver traffic ready ocean horror shaft foil miss code ribbon liberty glove';
 
-const web3: any = new Web3(new HDWalletProvider(mnemonic, providerUrl));
-
-async function main() {
-  const appRef = document.getElementById('app');
-  const address = await loadAccount(web3);
-  const provider = await loadProvider(web3, address);
-  await provider.getTitle();
-  render(React.createElement(App, {defaultProvider: provider, address, web3, defaultView: ViewsEnum.MAIN}, null), appRef);
-}
-
-main();
+render(createElement(App, {}, null), document.getElementById('app'));
+// const mnemonic = 'silver traffic ready ocean horror shaft foil miss code ribbon liberty glove';
 
